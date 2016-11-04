14e9998823b81a24ef8b4ed264be5a0e8eaec65b
Shaun Campbell <shaun.campbell@me.com>
2016-11-04 22:26:06 +0000

    Adding code to allow access to the push button Allowing the polling frequency to be configurable

--- index.js
+++ index.js
@@ -10,6 +10,34 @@ module.exports = function(homebridge) {
 	Characteristic = homebridge.hap.Characteristic;
 	var inherits = require('util').inherits;
 	
+	//	Create the characteristic for the button press id
+	Characteristic.ButtonPressed = function() {
+		Characteristic.call(this, 'Button Pressed','0000006E-0000-1000-8000-0037BB765301');
+		this.setProps({
+			format: Characteristic.Formats.INT,
+			unit: Characteristic.Units.NONE,
+			maxValue: 4,
+			minValue: 0,
+			minStep: 1,
+			perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+		});
+		this.value = this.getDefaultValue();
+	};
+	inherits(Characteristic.ButtonPressed, Characteristic);
+	Characteristic.ButtonPressed.UUID = '0000006E-0000-1000-8000-0037BB765301';
+	
+	//	Create the characteristic for the button name
+	Characteristic.ButtonName = function() {
+		Characteristic.call(this, 'Button Name','0000006E-0000-1000-8000-0037BB765302');
+		this.setProps({
+			format: Characteristic.Formats.STRING,
+			perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+		});
+		this.value = this.getDefaultValue();
+	};
+	inherits(Characteristic.ButtonName, Characteristic);
+	Characteristic.ButtonName.UUID = '0000006E-0000-1000-8000-0037BB765302';
+	
 	homebridge.registerAccessory("homebridge-hue-sensor-data", "HueSensor", HueSensorAccessory);
 }
 
@@ -20,6 +48,7 @@ function HueSensorAccessory(log, config, api) {
 	this.temperature = 0.0;
 	this.lightLevel = 0.0001;
 	this.presence = true;
+	this.switchBtn = 0;
 	
 	var platform = this;
 	
@@ -45,11 +74,13 @@ function HueSensorAccessory(log, config, api) {
 						platform.lightLevel = lightlevel;
 					} else if (config.sensors[uniqueid] === 'ZLLPresence') {
 						platform.presence = sensor.state.attributes.attributes.presence;
+					} else if (config.sensors[uniqueid] === 'ZLLSwitch') {
+						platform.switchBtn = sensor.state.attributes.attributes.buttonevent;
 					}
 				}
 			}
 		});
-	}, 10000);
+	}, config.refreshInterval);
 }
 
 //	Create custom characteristic.
@@ -71,6 +102,29 @@ HueSensorAccessory.prototype = {
 	
 	getPresence: function(callback) {
 		callback(null, this.presence);
+	},
+	getDimmerSwitchButtonPress: function(callback) {
+		var buttonStates = {
+			1000: 0, 1001: 0, 1002: 1, 1003: 1,
+			2000: 0, 2001: 0, 2002: 2, 2003: 2,
+			3000: 0, 3001: 0, 3002: 3, 3003: 3,
+			4000: 0, 4001: 0, 4002: 4, 4003: 4
+		};
+		var state = buttonStates[this.switchBtn];
+		callback(null, state);
+	},
+	getDimmerSwitchButtonName: function(callback) {
+		var buttonStates = {
+			1000: 'None', 1001: 'None', 1002: 'On', 1003: 'On',
+			2000: 'None', 2001: 'None', 2002: 'Up', 2003: 'Up',
+			3000: 'None', 3001: 'None', 3002: 'Down', 3003: 'Down',
+			4000: 'None', 4001: 'None', 4002: 'Off', 4003: 'Off'
+		};
+		var state = buttonStates[this.switchBtn];
+		callback(null, state);
+	},
+	getDimmerSwitchButtonActive: function(callback) {
+		callback(null, true);
 	},
 	getServices: function() {
 		
@@ -100,6 +154,22 @@ HueSensorAccessory.prototype = {
 				var sensor = new Service.OccupancySensor("Presence");
 				sensor.getCharacteristic(Characteristic.OccupancyDetected)
 				      .on('get', this.getPresence.bind(this));
+				sensors.push(sensor);
+			} else if (sensorType === 'ZLLSwitch') {
+				var sensor = new Service.Switch("Dimmer");
+				
+				sensor.addCharacteristic(Characteristic.ButtonPressed);
+				sensor.addCharacteristic(Characteristic.ButtonName);
+				
+				sensor.getCharacteristic(Characteristic.ButtonPressed)
+				      .on('get', this.getDimmerSwitchButtonPress.bind(this));
+				
+				sensor.getCharacteristic(Characteristic.ButtonName)
+				      .on('get', this.getDimmerSwitchButtonName.bind(this));
+					  
+				sensor.getCharacteristic(Characteristic.On)
+					  .on('get', this.getDimmerSwitchButtonActive.bind(this));
+				
 				sensors.push(sensor);
 			}
 		}
